e32bea241847ba91958f9bac875e5d3164832b1b
Xing.Wu <329106954@qq.com>
2023-08-08 23:01:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 cp /aoi/Guardian/guardian.phar /output
 cp /aoi/TapeWorm/tapeworm.phar /output
+cp /aoi/AoiAWD/aoiawd.phar /output
 cp /aoi/RoundWorm/roundworm /output
 cd /aoi/AoiAWD/
 php /aoi/AoiAWD/aoiawd.phar -m mongodb://mongodb:27017
